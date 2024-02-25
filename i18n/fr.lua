c941ceff5ca0057f4f680a8be7d71ae6d34a4719
serjoscha <acidnightmare@freenet.de>
2024-02-25 22:47:41 +0100

    french translations update + version inc.

--- i18n/fr.lua
+++ i18n/fr.lua
@@ -5,8 +5,8 @@ if GetLocale() == "frFR" then
         ["all spells on max rank"] = "Tous les sorts sur vos barres d'action sont au rang maximum !",
         ["uprank failed because spell not learned"] = "|cffFF0000Ce sort ne peut pas être amélioré car vous n'avez pas encore appris le nouveau rang. Veuillez consulter un entraineur et réessayer.|r",
         ["uprank successful"] = "%s |cffFF0000%s|r amélioré avec succès vers %s |cff00FF00%s|r!",
-        ["outranked spell used chat"] = "  %s |cffFF0000%s|r used.\nRang maximum disponible pour vous au  |cffFF0000niveau %d|r is:\n%s |cffFF0000%s|r!",
-        ["outranked spell used fading"] = "%s |cffFF0000%s|r used.\nRang maximum disponible pour vous au  |cffFF0000niveau %d|r is |cffFF0000%s|r!",
+        ["outranked spell used chat"] = " Utilisation de %s |cffFF0000%s|r détecté.\nUn rang suppérieur est disponible pour votre |cffFF0000niveau %d|r c'est:\n%s |cffFF0000%s|r!",
+        ["outranked spell used fading"] = "Utilisation de %s |cffFF0000%s|r Détecté.\nAmélioration disponible: |cffFF0000niveau %d|r vers le |cffFF0000%s|r!",
         ["info row chunks"] = {
             "peu être amélioré vers",
             "",
